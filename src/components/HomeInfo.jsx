0979f8d0d3f7f4015751af3ab67db5ef751f7e3d
rado-kolev <rado.k.kolev@gmail.com>
2023-12-13 21:41:46 +0000

    First commit

---
+++ src/components/HomeInfo.jsx
@@ -0,0 +1,51 @@
+import { Link } from 'react-router-dom';
+import { arrow } from '../assets/icons'
+
+const InfoBox = ({ text, link, btnText }) => {
+  return (
+    <div className='info-box'>
+      <p className='font-medium sm:text-lg text-center'>{text}</p>
+      <Link to={link} className='neo-btn neo-brutalism-white'>
+        {btnText}
+        <img src={arrow} />
+      </Link>
+    </div>
+  );
+}
+
+const renderContent = {
+  1: (
+    <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
+      Hi, I am <span className='font-semibold'>Rado</span>👋
+      <br />A Front-end Developer
+    </h1>
+  ),
+  2: (
+    <InfoBox
+      text='I Know That Good Site Means Good Business'
+      link='/about'
+      btnText='Learn More'
+    />
+  ),
+  3: (
+    <InfoBox
+      text='My Creative Portfolio Section'
+      link='/projects'
+      btnText='Visit it'
+    />
+  ),
+  4: (
+    <InfoBox
+      text='Take A Coffee & Chat With Me'
+      link='/contact'
+      btnText="Let's talk"
+    />
+  ),
+};
+
+
+const HomeInfo = ({ currentStage }) => {
+  return renderContent[currentStage] || null;
+}
+
+export default HomeInfo
---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+
+
+const About = () => {
+  return (
+    <section className='max-container'>
+
+    </section>
+  )
+}
+
+export default About
